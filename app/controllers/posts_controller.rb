0b8127d43e3c483460c656663acd9a2ab08f476f
navase <margot.sauter@gmail.com>
2016-02-05 12:28:18 +0100

    Create and List  Posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,26 @@
 class PostsController < ApplicationController
+  # A list of al posts
   def index
+    @posts = Post.all
+  end
+
+  # An empty Post form
+  def new
+    @post = Post.new
+  end
+
+  # Create a Post from the form params
+  def create
+    if @post = Post.create(post_params)
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
   end
 end
